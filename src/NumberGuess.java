7fb35d8ca181340def38c40531f574121bdd4596
Davis Crain <dhcrain@gmail.com>
2016-12-06 13:41:14 -0500

    starting to work on number guesser

---
+++ src/NumberGuess.java
@@ -0,0 +1,11 @@
+/**
+ * Created by daviscrain on 12/6/16.
+ */
+
+
+public class NumberGuess {
+    public static void main (String[] args) {
+        System.out.println("Guess a number form 1, 100");
+    }
+
+}
