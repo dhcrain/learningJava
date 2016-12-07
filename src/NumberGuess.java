37a2b3ef78c7e1e4c7e0ff6ce9a811b7cb750b6f
Davis Crain <dhcrain@gmail.com>
2016-12-06 21:47:46 -0500

    number guesser is a go

--- src/NumberGuess.java
+++ src/NumberGuess.java
@@ -1,3 +1,8 @@
+import java.util.Random;
+import java.util.Scanner;
+
+import static java.lang.Math.abs;
+
 /**
  * Created by daviscrain on 12/6/16.
  */
@@ -5,7 +10,43 @@
 
 public class NumberGuess {
     public static void main (String[] args) {
-        System.out.println("Guess a number form 1, 100");
+        // Set-up
+        int guesses = 5;
+        int MAX = 100;
+        int MIN = 1;
+        int guess = -1;
+        Random ran = new Random();
+        int randomNumber = ran.nextInt((MAX - MIN) + 1) + MIN;
+        System.out.printf("You have %s guesses to guess a number between %s and %s\n\n", guesses, MIN, MAX);
+
+        // create a scanner to read cl input
+        Scanner scanner = new Scanner(System.in);
+
+        while (guess != randomNumber && guesses > 0) {
+//            System.out.print(randomNumber);
+            System.out.print("What is your guess?  ");
+            String guessStr = scanner.next();
+            try {
+                guess = Integer.parseInt(guessStr);
+                guesses--;
+
+                if (guess == randomNumber) {
+                    System.out.printf("You Win! the number was %s\n", randomNumber);
+                } else {
+                    if (guess < randomNumber) {
+                        System.out.print("Too Low!  ");
+                    } else {
+                        System.out.print("Too high!  ");
+                    }
+                    if (abs(guess - randomNumber) <= 5) {
+                        System.out.print("Your Getting Close!\n");
+                    }
+                    System.out.printf("You have %s guesses left\n", guesses);
+                }
+            } catch (NumberFormatException nfe) {
+                System.out.print("That is not a number guess again!  \n");
+            }
+        }
     }
 
 }
